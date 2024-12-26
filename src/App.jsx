233f09bb2a275ed9cc0fa207145c134e1ce07d3f
kelixirr <kelixirr@gmail.com>
2024-12-26 15:45:11 +0530

    Project Initiated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Details from './components/kelixirr/Details';
+import Hero from './Hero' 
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Details />
+    </>
+  );
+}
+
+export default App
