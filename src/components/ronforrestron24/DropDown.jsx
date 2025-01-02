f693e64f1be034e19900c84408c35a3594624c10
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-02 20:53:30 +0800

    Drop down menu bar by Ron Manguerra

--- src/components/ronforrestron24/DropDown.jsx
+++ src/components/ronforrestron24/DropDown.jsx
@@ -18,6 +18,7 @@ function DropDown() {
           <option value="option1">Option 1</option>
           <option value="option2">Option 2</option>
           <option value="option3">Option 3</option>
+          <option value="option4">Option 4</option>
         </select>
         <div className="author">
           <Author
